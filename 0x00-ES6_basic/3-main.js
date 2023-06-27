4c971dab1a8eddf4558639eb23a559693cb2a90c
leroysb <leroybuliro@gmail.com>
2023-06-27 17:45:12 +0300

    task3

---
+++ 0x00-ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+import getSumOfHoods from './3-default-parameter';
+
+console.log(getSumOfHoods(34)); // eslint-disable-line
+console.log(getSumOfHoods(34, 3)); // eslint-disable-line
+console.log(getSumOfHoods(34, 3, 4)); // eslint-disable-line
