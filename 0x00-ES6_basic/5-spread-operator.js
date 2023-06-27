b4750d85ac21379bf4668d0868e72fa32702884f
leroysb <leroybuliro@gmail.com>
2023-06-27 19:03:31 +0300

    task5final

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-  const listArray = [...array1, ...array2, ...Array.from(string)];
-  return listArray;
+  return [...array1, ...array2, ...Array.from(string)];
 }
