f9f44d79747639a37389ecea61a63630ed8e4f6f
leroysb <leroybuliro@gmail.com>
2023-06-27 18:12:10 +0300

    task4final

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...Args) {
-  let total = 0;
-  for (const arg of Args) { // eslint-disable-line
-    total += 1;
-  }
-  return total;
+  return Args.length;
 }
