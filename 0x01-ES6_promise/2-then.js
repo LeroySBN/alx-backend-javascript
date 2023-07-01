853ba84470d07f8afe3566b2b49079ff9101b529
leroysb <leroybuliro@gmail.com>
2023-07-01 23:21:32 +0300

    task2 bug fix 4

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
     .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
