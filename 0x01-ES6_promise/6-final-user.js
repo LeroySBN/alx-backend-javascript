8753d4ee38e759531c9223662b39d1370df36441
leroysb <leroybuliro@gmail.com>
2023-07-02 14:52:06 +0300

    task6 refactored

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,27 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = signUpUser(firstName, lastName);
-  const promise2 = uploadPhoto(fileName);
-  return Promise.allSettled([promise1, promise2])
-    .then((values) => console.log(values));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const returnObj = [];
+  try {
+    const promise1 = await signUpUser(firstName, lastName);
+    returnObj.push({ status: 'fulfilled', value: promise1 });
+  } catch (err) {
+    returnObj.push({
+      status: 'rejected',
+      value: err.toString(),
+    });
+  }
+
+  try {
+    const promise2 = await uploadPhoto(fileName);
+    returnObj.push({ status: 'fulfilled', value: promise2 });
+  } catch (err) {
+    returnObj.push({
+      status: 'rejected',
+      value: err.toString(),
+    });
+  }
+
+  return returnObj;
 }
