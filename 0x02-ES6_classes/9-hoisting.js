5c7f5c06995622fe4161eade8b29b0e773dfe8ed
leroysb <leroybuliro@gmail.com>
2023-07-03 00:25:19 +0300

    task9 bug fix2

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -4,11 +4,14 @@ export class HolbertonClass {
     this._location = location;
   }
 
-  get year() { return this._year; }
+  get year() {
+    return this._year;
+  }
 
-  get location() { return this._location; }
+  get location() {
+    return this._location;
+  }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -19,15 +22,18 @@ export class StudentHolberton {
     this._holbertonClass = holbertonClass;
   }
 
-  get fullName() { return `${this._firstName} ${this._lastName}`; }
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
 
-  get holbertonClass() { return this.holbertonClass; }
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
