087b1b7ee1d23f6a2da42133c03b3233bf7809fd
leroysb <leroybuliro@gmail.com>
2023-07-05 04:31:06 +0300

    task11

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  let called = weakMap.get(endpoint) || 0;
+  called += 1;
+  weakMap.set(endpoint, called);
+
+  if (called >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  return called;
+};
+
+export { queryAPI, weakMap };
