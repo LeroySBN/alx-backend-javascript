daff747291364e178445200740865d2d8d433f8f
leroysb <leroybuliro@gmail.com>
2023-08-22 18:25:26 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,8 +8,7 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
         return;
       }
-
-      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
       const header = lines.shift();
       const headers = header.split(',');
       const fieldIndex = headers.indexOf('field');
@@ -19,7 +18,6 @@ function countStudents(path) {
         const columns = line.split(',');
         const field = columns[fieldIndex];
         const firstname = columns[firstnameIndex];
-
         if (!studentsByField[field]) {
           studentsByField[field] = [];
         }
@@ -29,7 +27,6 @@ function countStudents(path) {
       Object.entries(studentsByField).forEach(([field, students]) => {
         console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
       });
-
       resolve(lines.length);
     });
   });
