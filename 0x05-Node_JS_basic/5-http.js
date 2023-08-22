559f005b445c99c995cd428d8a936ae801d3a50f
leroysb <leroybuliro@gmail.com>
2023-08-22 23:17:53 +0300

    Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,19 +4,25 @@ const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Holberton School!');
+    res.end('Hello Holberton School!\n');
   } else if (req.url === '/students') {
-    try {
-      const studentsInfo = countStudents(process.argv[2]);
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(`${studentsInfo}`);
-    } catch (err) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error\n');
-    }
+    const databasePath = process.argv[2];
+    await countStudents(databasePath)
+      .then((studentsInfo) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        const responseContent = `This is the list of our students\n${studentsInfo}`;
+        res.end(responseContent);
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
   }
 });
 
