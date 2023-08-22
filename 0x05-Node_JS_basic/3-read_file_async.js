690ee470a101e944870887ef726dff5ddacc2b8c
leroysb <leroybuliro@gmail.com>
2023-08-22 10:50:40 +0300

    Reading a file asynchronously with Node Js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,30 @@
+// Reading a file asynchronously with Node JS
+const fs = require('fs');
+
+module.exports = function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    let lines = data.split('\n');
+    // const fields = lines[0].split(',');
+    // console.log(fields);
+    lines = lines.slice(1, lines.length);
+    // console.log(lines);
+    console.log(`Number of students: ${lines.length}`);
+    const students = {};
+    for (const line of lines) {
+      if (line.length > 0) {
+        const student = line.split(',');
+        const field = student[3];
+        if (!students[field]) {
+          students[field] = [];
+        }
+        students[field].push(student[0]);
+      }
+    }
+    // console.log(students);
+    console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
+    console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+};
