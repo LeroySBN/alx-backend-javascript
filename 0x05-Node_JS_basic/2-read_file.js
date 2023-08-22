b3611d93fc0411722b449ea0e398fbe836208d6d
leroysb <leroybuliro@gmail.com>
2023-08-22 18:15:03 +0300

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,28 +4,21 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    console.log(data);
     const lines = data.split('\n').filter((line) => line.trim() !== ''); // Remove empty lines
-    console.log(lines);
     const header = lines.shift();
-    console.log(header);
     const headers = header.split(',');
     const fieldIndex = headers.indexOf('field');
-    console.log(fieldIndex);
     const firstnameIndex = headers.indexOf('firstname');
     const studentsByField = {};
     lines.forEach((line) => {
       const columns = line.split(',');
-      console.log(columns);
       const field = columns[fieldIndex];
-      console.log(field);
       const firstname = columns[firstnameIndex];
       if (!studentsByField[field]) {
         studentsByField[field] = [];
       }
       studentsByField[field].push(firstname);
     });
-    console.log('====================================');
     console.log(`Number of students: ${lines.length}`);
     Object.entries(studentsByField).forEach(([field, students]) => {
       console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
