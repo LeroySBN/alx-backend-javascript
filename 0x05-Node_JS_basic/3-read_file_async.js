8ba8934fffaa6295ad6c7943b19179dca0da5fb6
leroysb <leroybuliro@gmail.com>
2023-08-22 18:24:00 +0300

    Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,38 @@
 // Reading a file asynchronously with Node JS
 const fs = require('fs');
 
-module.exports = function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf8');
-    let lines = data.split('\n');
-    // const fields = lines[0].split(',');
-    // console.log(fields);
-    lines = lines.slice(1, lines.length);
-    // console.log(lines);
-    console.log(`Number of students: ${lines.length}`);
-    const students = {};
-    for (const line of lines) {
-      if (line.length > 0) {
-        const student = line.split(',');
-        const field = student[3];
-        if (!students[field]) {
-          students[field] = [];
-        }
-        students[field].push(student[0]);
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
       }
-    }
-    // console.log(students);
-    console.log(`Number of students in CS: ${students.CS.length}. List: ${students.CS.join(', ')}`);
-    console.log(`Number of students in SWE: ${students.SWE.length}. List: ${students.SWE.join(', ')}`);
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
-};
+
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const header = lines.shift();
+      const headers = header.split(',');
+      const fieldIndex = headers.indexOf('field');
+      const firstnameIndex = headers.indexOf('firstname');
+      const studentsByField = {};
+      lines.forEach((line) => {
+        const columns = line.split(',');
+        const field = columns[fieldIndex];
+        const firstname = columns[firstnameIndex];
+
+        if (!studentsByField[field]) {
+          studentsByField[field] = [];
+        }
+        studentsByField[field].push(firstname);
+      });
+      console.log(`Number of students: ${lines.length}`);
+      Object.entries(studentsByField).forEach(([field, students]) => {
+        console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+      });
+
+      resolve(lines.length);
+    });
+  });
+}
+
+module.exports = countStudents;
