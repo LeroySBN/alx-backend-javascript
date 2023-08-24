48d8efdc8f23dca1a0bb94776f0142505aa65b81
leroysb <leroybuliro@gmail.com>
2023-08-25 02:36:04 +0300

    Basic Integration testing

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,27 +1,15 @@
 // Test cases for module api
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api');
+const request = require('request');
+const { expect } = require('chai');
 
-chai.use(chaiHttp);
-const expect = chai.expect;
+describe('Basic integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index Page', () => {
-  it('responds with status code 200', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('responds with correct result', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
