c23957e0011fc08cbb6580b8a4168c85788d62f1
leroysb <leroybuliro@gmail.com>
2023-08-25 01:48:21 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -2,6 +2,8 @@
 const getPaymentTokenFromAPI = (success) => {
   if (success) {
     return Promise.resolve({data: 'Successful response from the API'});
+  } else {
+    return Promise.reject(new Error('Request failed'));
   }
 };
 
