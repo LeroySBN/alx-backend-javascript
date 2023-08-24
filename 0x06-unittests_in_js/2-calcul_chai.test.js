d843a94e8a3fb12e35e2e7768fc5c5e18eb01979
leroysb <leroybuliro@gmail.com>
2023-08-24 23:24:36 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -6,43 +6,35 @@ const calculateNumber = require('./1-calcul'); // Replace with the actual path t
 
 describe('calculateNumber', () => {
   it('should add two rounded positive numbers when type is SUM', () => {
-    const result = calculateNumber('SUM', 3.7, 2.1);
-    expect(result).to.equal(6);
+    expect(calculateNumber('SUM', 3.7, 2.1)).to.equal(6);
 
   });
 
   it('should subtract b from a when type is SUBTRACT', () => {
-    const result = calculateNumber('SUBTRACT', 5.6, 2.8);
-    expect(result).to.equal(3);
+    expect(calculateNumber('SUBTRACT', 5.6, 2.8)).to.equal(3);
   });
 
   it('should divide a by b when type is DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 7.5, 2);
-    expect(result).to.equal(4);
+    expect(calculateNumber('DIVIDE', 7.5, 2)).to.equal(4);
   });
 
   it('should return "Error" if b is rounded to 0 and type is DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 10, 0.2);
-    expect(result).to.equal('Error');
+    expect(calculateNumber('DIVIDE', 10, 0.2)).to.equal('Error');
   });
 
   it('should throw an error for invalid type', () => {
-    const result = calculateNumber('INVALID', 1, 2);
-    expect(result).to.equal('Invalid type');
+    expect(() => calculateNumber('INVALID', 1, 2)).to.throw('Invalid type');
   });
 
   it('should add two rounded negative numbers when type is SUM', () => {
-    const result = calculateNumber('SUM', -3.7, -2.1);
-    expect(result).to.equal(-6);
+    expect(calculateNumber('SUM', -3.7, -2.1)).to.equal(-6);
   });
 
   it('should subtract a from b when type is SUBTRACT', () => {
-    const result = calculateNumber('SUBTRACT', 2.8, 5.6);
-    expect(result).to.equal(-3);
+    expect(calculateNumber('SUBTRACT', 2.8, 5.6)).to.equal(-3);
   });
 
   it('should divide b by a when type is DIVIDE', () => {
-    const result = calculateNumber('DIVIDE', 2, 7.5);
-    expect(result).to.equal(0.25);
+    expect(calculateNumber('DIVIDE', 2, 7.5)).to.equal(0.25);
   });
 });
