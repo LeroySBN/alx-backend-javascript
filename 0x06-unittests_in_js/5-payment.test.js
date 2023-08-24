5c2d9618b82d8323864898cc8cbd9324bc4905d0
leroysb <leroybuliro@gmail.com>
2023-08-25 00:33:14 +0300

    Hooks

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,34 +1,30 @@
 const sinon = require('sinon');
-const assert = require('assert');
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('should call Utils.calculateNumber with arguments 100 and 20 and log the result', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    calculateNumberStub.withArgs('SUM', 100, 20).returns(120);
-    const consoleLogSpy = sinon.spy(console, 'log');
+  let consoleSpy;
 
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should call Utils.calculateNumber with arguments 100 and 20 and log the result', () => {
     sendPaymentRequestToApi(100, 20);
 
-    assert.strictEqual(calculateNumberStub.calledWith('SUM', 100, 20), true);
-    assert.strictEqual(consoleLogSpy.calledWith('The total is: 120'), true);
-
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
   });
 
   it('should call Utils.calculateNumber with arguments 10 and 10 and log the result', () => {
-    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
-    calculateNumberStub.withArgs('SUM', 10, 10).returns(20);
-    const consoleLogSpy = sinon.spy(console, 'log');
-
     sendPaymentRequestToApi(10, 10);
 
-    assert.strictEqual(calculateNumberStub.calledWith('SUM', 10, 10), true);
-    assert.strictEqual(consoleLogSpy.calledWith('The total is: 20'), true);
-
-    calculateNumberStub.restore();
-    consoleLogSpy.restore();
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWith('The total is: 20')).to.be.true;
   });
 });
